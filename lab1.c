6d9491c2f55a32652aa720c33429faf7697cebc0
phosphor185 <sunpiexd@gmail.com>
2026-03-14 09:22:00 +1000

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,1 +1,31 @@
-gfrgegf
+#include <stdio.h>
+#include <math.h>
+int main() {
+ double a;
+    printf("vvedite a: ");
+    scanf ("%lf", &a);
+    double b;
+    printf("vvedite b");
+    scanf ("%lf", &b);
+    double c;
+    printf("vvedite c");
+    scanf ("%lf", &c);
+    double d = pow (b, 2) - 4 * a * c;
+    
+    if (d < 0){
+     printf ("d < 0");
+    }
+    if (d == 0){
+  double x1 = (-b / 2 * a);
+  printf ("x1 = %lf \n", x1);
+  printf ("D = %lf \n", d);
+ }
+ if (d > 0){
+     double x1 = (-b + pow(d, 0.5))/(2*a);
+     double x2 = (-b - pow(d, 0.5))/(2*a);
+     printf ("x1 = %lf \n", x1);
+     printf ("x2 = %lf \n", x2);
+     printf ("D = %lf \n", d);
+    }
+ return 0;
+}
