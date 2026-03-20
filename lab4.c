7c3ee1babf7fba930e6cd16bbbb8b8bd4056cf33
phosphor185 <sunpiexd@gmail.com>
2026-03-21 09:42:45 +1000

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,18 +1,39 @@
+#include <stdio.h>
 #include "triangle.h"
-#include <math.h>
 
-// Проверка неравенства треугольника
-int is_valid_triangle(double a, double b, double c) {
-    return (a + b > c) && (a + c > b) && (b + c > a);
-}
-
-// Вычисление периметра
-double calculate_perimeter(double a, double b, double c) {
-    return a + b + c;
-}
-
-// Вычисление площади по формуле Герона
-double calculate_area(double a, double b, double c) {
-    double p = calculate_perimeter(a, b, c) / 2.0; // полупериметр
-    return sqrt(p * (p - a) * (p - b) * (p - c));
+int main() {
+    double a, b, c;
+    
+    printf("Введите длины сторон треугольника:\n");
+    printf("Сторона a = ");
+    scanf("%lf", &a);
+    
+    printf("Сторона b = ");
+    scanf("%lf", &b);
+    
+    printf("Сторона c = ");
+    scanf("%lf", &c);
+    
+    // Проверка на положительность
+    if (a <= 0 || b <= 0 || c <= 0) {
+        printf("Ошибка: длины сторон должны быть положительными числами!\n");
+        return 1;
+    }
+    
+    // Проверка неравенства треугольника
+    if (!is_valid_triangle(a, b, c)) {
+        printf("Ошибка: треугольник с такими сторонами не существует!\n");
+        printf("Нарушено неравенство треугольника.\n");
+        return 1;
+    }
+    
+    // Вычисление и вывод результатов
+    double perimeter = calculate_perimeter(a, b, c);
+    double area = calculate_area(a, b, c);
+    
+    printf("\nРезультаты:\n");
+    printf("Периметр треугольника: %.2f\n", perimeter);
+    printf("Площадь треугольника: %.2f\n", area);
+    
+    return 0;
 }
