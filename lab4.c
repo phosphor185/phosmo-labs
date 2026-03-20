5a2041c0a6fb175cdc113ea5046614a57f89c00d
phosphor185 <sunpiexd@gmail.com>
2026-03-21 09:45:58 +1000

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,39 +1,15 @@
-#include <stdio.h>
-#include "triangle.h"
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
 
-int main() {
-    double a, b, c;
-    
-    printf("Введите длины сторон треугольника:\n");
-    printf("Сторона a = ");
-    scanf("%lf", &a);
-    
-    printf("Сторона b = ");
-    scanf("%lf", &b);
-    
-    printf("Сторона c = ");
-    scanf("%lf", &c);
-    
-    // Проверка на положительность
-    if (a <= 0 || b <= 0 || c <= 0) {
-        printf("Ошибка: длины сторон должны быть положительными числами!\n");
-        return 1;
-    }
-    
-    // Проверка неравенства треугольника
-    if (!is_valid_triangle(a, b, c)) {
-        printf("Ошибка: треугольник с такими сторонами не существует!\n");
-        printf("Нарушено неравенство треугольника.\n");
-        return 1;
-    }
-    
-    // Вычисление и вывод результатов
-    double perimeter = calculate_perimeter(a, b, c);
-    double area = calculate_area(a, b, c);
-    
-    printf("\nРезультаты:\n");
-    printf("Периметр треугольника: %.2f\n", perimeter);
-    printf("Площадь треугольника: %.2f\n", area);
-    
+// Функция вычисления периметра треугольника
+double calculate_perimeter(double a, double b, double c);
+
+// Функция вычисления площади треугольника (формула Герона)
+double calculate_area(double a, double b, double c);
+
+// Функция проверки существования треугольника
+int is_valid_triangle(double a, double b, double c);
+
+#endif    
     return 0;
 }
