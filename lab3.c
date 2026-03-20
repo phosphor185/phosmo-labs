1deb0be8c2767ca915882bd81cf075c0ff54d17d
phosphor185 <sunpiexd@gmail.com>
2026-03-21 09:14:04 +1000

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 
 int main() {
+    char str[81];
+    int i = 0;
+    
+    // Ввод строки с консоли
+    printf("Введите строку (до 80 символов): ");
+    fgets(str, sizeof(str), stdin);
+    
+    // Замена 'a' на 'A' и 'b' на 'B'
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a') {
+            str[i] = 'A';
+        } else if (str[i] == 'b') {
+            str[i] = 'B';
+        }
+    }
+    
+    // Вывод результата
+    printf("Результат: %s\n", str);
+    
+    return 0;
+}
