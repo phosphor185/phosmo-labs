2ece642205d99098a0597db272ddaf9a10254c16
phosphor185 <sunpiexd@gmail.com>
2026-04-25 09:40:52 +1000

    Create lab7.c

---
+++ lab7.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct humen {
+    char surname[50];
+    char name[50];
+    int year;
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    
+    struct humen arr1[4], arr2[4];
+    struct humen temp;
+
+    printf("Vvedi 4 cheloveka (Familiya Imya God):\n");
+    for (int i = 0; i < 4; i++) {
+        printf("Chelovek %d: ", i + 1);
+        scanf("%s %s %d", arr1[i].surname, arr1[i].name, &arr1[i].year);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        arr2[i] = arr1[i];
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3 - i; j++) {
+            if (arr2[j].year > arr2[j+1].year) {
+                temp = arr2[j];
+                arr2[j] = arr2[j+1];
+                arr2[j+1] = temp;
+            }
+        }
+    }
+
+    printf("\nOtsortirovano po godu rozhdeniya:\n");
+    for (int i = 0; i < 4; i++) {
+        printf("%s %s %d\n", arr2[i].surname, arr2[i].name, arr2[i].year);
+    }
+
+    return 0;
+}
